1cc68f24aebf8de1fa6dede34d870aad89b82e5e
Sam Harradine <124873582+Equallizerr1@users.noreply.github.com>
2024-01-04 20:42:33 +0000

    created rng functions

--- Horse or Laminator/scripts.js
+++ Horse or Laminator/scripts.js
@@ -1,11 +1,41 @@
+function buttonClick(clickedButton) {
+	function innerButton() {
+		const choice = clickedButton;
+		function rng() {
+			let counter = 0;
+			const choiceArray = ["horse", "laminator"];
+			const choiceRng = choiceArray[(choiceArray.length * Math.random()) | 0];
+			if (choice === choiceRng) {
+				return true;
+			} else return false;
+		}
+		return rng();
+	}
+	return innerButton();
+}
+
+buttonClick("horse");
+console.log(buttonClick("horse"));
+buttonClick("horse");
+buttonClick("laminator");
+
+// const horseButtonObject = document.getElementById("horse");
+// const horseButton = horseButtonObject.id;
+// buttonClick(horseButton);
+
+//const selectedButton = buttonClick(document.getElementById("horse"));
+//console.log(selectedButton);
+
 function rng() {
 	const choiceArray = ["horse", "laminator"];
 	const choiceRng = choiceArray[(choiceArray.length * Math.random()) | 0];
 	return choiceRng;
 }
+// const rngFunc = rng();
+// console.log(rngFunc);
 
-function buttonClick(clicked_id) {
-	let button = document.getElementById(clicked_id);
-	console.log(clicked_id);
-	return button;
-}
+// const answerChecker = (rngFunc, selectedButton) => {
+// 	//console.log(rngFunc, selectedButton);
+// };
+
+// answerChecker(rngFunc, selectedButton);
